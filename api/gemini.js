b76cd0fe6a2305d4be35c5637ca870c139b82765
intel051 <ppodolukr@gmail.com>
2026-05-31 19:52:07 +0900

    gemini.js 업데이트

--- api/gemini.js
+++ api/gemini.js
@@ -24,7 +24,7 @@ export default async function handler(req) {
       });
     }
 
-    const endpoint = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${apiKey}`;
+    const endpoint = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${apiKey}`;
 
     let payload = {};
 
